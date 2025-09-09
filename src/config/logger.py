42dcbcc8b6d44eb404e7cc7c9e716c10ec4f553d
Lewis Halstead <lewishalstead5@gmail.com>
2025-09-09 20:36:42 +0100

    add logger and settings

---
+++ src/config/logger.py
@@ -0,0 +1,20 @@
+import logging
+
+from rich.logging import RichHandler
+
+from .settings import settings
+
+FORMAT = "| %(filename)s:%(lineno)d >>> %(message)s"
+
+
+def setup_logging():
+    logging.basicConfig(
+        level=settings.LOG_LEVEL,
+        format=FORMAT,
+        datefmt="[%X]",
+        handlers=[RichHandler()],
+    )
+
+
+setup_logging()
+logger = logging.getLogger("backend")
