d4d2682b082d83dac2d2941e332087bcb49c5500
Lewis Halstead <lewishalstead5@gmail.com>
2025-09-13 21:35:53 +0100

    add alembic for db migration

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "DEBUG"
     DB_PASSWORD: SecretStr = SecretStr("")
     DB_USERNAME: SecretStr = SecretStr("")
+    SQLALCHEMY_URL: SecretStr = SecretStr("")
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
